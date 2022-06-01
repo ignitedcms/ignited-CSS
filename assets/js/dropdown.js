3b4210786bb7e50ea49d683f2bebf6e9d0b0f191
ignitedcms <iamthwee@gmail.com>
2022-06-01 22:27:36 +0100

    u

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,34 +1,32 @@
+/*
+|---------------------------------------------------------------
+| Components only data must be passed as a function
+| Use slots to repeat child components
+| Use props to pass in data 
+|---------------------------------------------------------------
+|
+|
+*/
 Vue.component('drop-down',{
     template: 
     `
-    <button class="btn btn-white drop-shadow" v-on:click="show =!show" v-on-clickaway="away">Click me
-                        <div v-if="show" class="dropdown br drop-shadow fade-in-bottom">
-                            
-                           <slot></slot> 
-                            
-                            
-                        </div>
-
-                    </button>
+    <button class="btn btn-white drop-shadow" v-on:click="show =!show" v-click-outside="away">Click me {{show}}
+        <div v-if="show" class="dropdown br drop-shadow fade-in-bottom">
+            <slot></slot> 
+        </div>
+    </button>
     `,
     data:function(){
 
         return{
-            mixins: [VueClickaway.mixin],
             message: 'Hello',
             show: false,
-            // methods: {
-            //     away: function () {
-            //         this.show = false;
-            //     },
-            // }
         }
     },
     methods:{
         away: function () {
             this.show = false;
         },
-
     } 
 });
 
@@ -44,27 +42,44 @@ Vue.component('item',{
         </div>
     </div>
     `,
-    data:function(){
+    // data:function(){
 
-        return{
+    //     return{
 
-        }
-    }
+    //     }
+    // }
 });
 
+/*
+|---------------------------------------------------------------
+| Special directive for click outside
+|---------------------------------------------------------------
+|
+| https://stackoverflow.com/questions/36170425/detect-click-outside-element
+*/
 
+Vue.directive('click-outside', {
+    bind: function (el, binding, vnode) {
+      this.event = function (event) {
+        if (!(el == event.target || el.contains(event.target))) {
+          vnode.context[binding.expression](event);
+        }
+      };
+      document.body.addEventListener('click', this.event)
+    },
+    unbind: function (el) {
+      document.body.removeEventListener('click', this.event)
+    },
+  });
 
+/*
+|---------------------------------------------------------------
+| Base entry for vue app
+|---------------------------------------------------------------
+|
+|
+*/
 
 var app = new Vue({
     el: '#app',
-    // mixins: [VueClickaway.mixin],
-    // data: {
-    //     message: 'Hello',
-    //     show: false,
-    // },
-    // methods: {
-    //     away: function () {
-    //         this.show = false;
-    //     }
-    // }
 })
