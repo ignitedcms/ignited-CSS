57a6d43c4c0ba5b9b4c72bc870a60f2db3be8d16
ignitedcms <iamthwee@gmail.com>
2022-06-01 22:30:35 +0100

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -2,7 +2,7 @@
 |---------------------------------------------------------------
 | Components only data must be passed as a function
 | Use slots to repeat child components
-| Use props to pass in data 
+| Use props to pass in data MUST use kebab case eg postTitle => post-title 
 |---------------------------------------------------------------
 |
 |
