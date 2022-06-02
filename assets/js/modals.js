fadf9ef452210b66a6c305aed076a4d139473048
ignitedcms <iamthwee@gmail.com>
2022-06-02 13:03:44 +0100

    first commit to modals

---
+++ assets/js/modals.js
@@ -0,0 +1,62 @@
+/*
+|---------------------------------------------------------------
+| Modals 
+| 
+| Components only data must be passed as a function
+| Use slots to repeat child components
+| Use props to pass in data MUST use kebab case eg postTitle => post-title 
+|---------------------------------------------------------------
+|
+|
+*/
+Vue.component('modal',{
+    props:['title'],
+    template: 
+    `
+    <div>
+        iamamodal
+    </div>
+    `,
+    data:function(){
+
+        return{
+            message: 'Hello',
+            show: false,
+        }
+    }
+});
+
+
+
+/*
+|---------------------------------------------------------------
+| Special directive for click outside
+|---------------------------------------------------------------
+|
+| https://stackoverflow.com/questions/36170425/detect-click-outside-element
+*/
+
+Vue.directive('click-outside', {
+    bind: function (el, binding, vnode) {
+      this.event = function (event) {
+        if (!(el == event.target || el.contains(event.target))) {
+          vnode.context[binding.expression](event);
+        }
+      };
+      document.body.addEventListener('click', this.event)
+    },
+    unbind: function (el) {
+      document.body.removeEventListener('click', this.event)
+    },
+  });
+
+/*
+|---------------------------------------------------------------
+| Base entry for vue app
+|---------------------------------------------------------------
+|
+*/
+
+var app = new Vue({
+    el: '#app',
+})
