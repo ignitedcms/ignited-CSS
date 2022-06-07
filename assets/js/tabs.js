9ac9c819eb0919ca96507f5b315949d2c499bc31
ignitedcms <iamthwee@gmail.com>
2022-06-07 17:43:50 +0100

    u

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -9,43 +9,58 @@
 | https://learnvue.co/2019/12/building-reusable-components-in-vuejs-tabs/#final-reusable-component-files
 |
 */
-Vue.component('tabs',{
-    template: 
-    `
+Vue.component('tabs', {
+  template: `
         <div>
-        <li v-for='(tab, index) in tabs'>
+        <li v-for='(tab, index) in tabs'
+        @click='selectTab(index)'
+        v-bind:class='{"tab__selected": (index == selectedIndex)}'
+        >
          {{ tab.title }}
         </li>
              <slot></slot> 
         </div>
     `,
-    data:function(){
+  data: function () {
 
-        return{
-          selectedIndex: 0,
-          tabs: []        
-        }
-    },
-    created () {
-      this.tabs = this.$children
-  	}
+    return {
+      selectedIndex: 0,
+      tabs: []
+    }
+  },
+  created() {
+    this.tabs = this.$children
+  },
+  mounted() {
+    this.selectTab(0)
+  },
+  methods: {
+    selectTab(i) {
+      this.selectedIndex = i
+      // loop over all the tabs
+      this.tabs.forEach((tab, index) => {
+        tab.is_active = (index === i)
+      })
+    }
+  }
 });
 
-Vue.component('tab-item',{
-    props: ['title'],
-    template: 
-    `
+Vue.component('tab-item', {
+  props: ['title'],
+  template: `
     <div class='tab' v-show='is_active'>
       <slot></slot>
     </div>
     `,
-    data:function(){
+  data: function () {
 
-        return{
-          is_active:true
-        }
+    return {
+      is_active: true
     }
+  }
 });
+
+
 
 /*
 |---------------------------------------------------------------
@@ -56,18 +71,18 @@ Vue.component('tab-item',{
 */
 
 Vue.directive('click-outside', {
-    bind: function (el, binding, vnode) {
-      this.event = function (event) {
-        if (!(el == event.target || el.contains(event.target))) {
-          vnode.context[binding.expression](event);
-        }
-      };
-      document.body.addEventListener('click', this.event)
-    },
-    unbind: function (el) {
-      document.body.removeEventListener('click', this.event)
-    },
-  });
+  bind: function (el, binding, vnode) {
+    this.event = function (event) {
+      if (!(el == event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener('click', this.event)
+  },
+  unbind: function (el) {
+    document.body.removeEventListener('click', this.event)
+  },
+});
 
 /*
 |---------------------------------------------------------------
@@ -78,5 +93,5 @@ Vue.directive('click-outside', {
 */
 
 var app = new Vue({
-    el: '#app',
+  el: '#app',
 })
