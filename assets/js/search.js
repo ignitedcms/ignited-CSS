ab82913792c22941f5c436039261874754550021
ignitedcms <iamthwee@gmail.com>
2022-06-08 10:04:57 +0100

    add first search bar attemp
    added base html
    added base css
    added base js

---
+++ assets/js/search.js
@@ -0,0 +1,69 @@
+/*
+|---------------------------------------------------------------
+| Search component 
+| 
+| Components only data must be passed as a function
+| Use slots to repeat child components
+| Use props to pass in data MUST use kebab case eg postTitle => post-title 
+|---------------------------------------------------------------
+|
+|
+*/
+Vue.component('search',{
+    template: 
+    `
+    <button class="btn rm-btn-styles tooltip-rel" v-on:click="show =!show" v-click-outside="away">
+     <span class="tooltip-highlight"> Click me </span>
+        <div v-if="show" class="tooltip   br drop-shadow fade-in">
+            <slot></slot> 
+        </div>
+    </button>
+    `,
+    data:function(){
+
+        return{
+            message: 'Hello',
+            show: false,
+        }
+    },
+    methods:{
+        away: function () {
+            this.show = false;
+        },
+    } 
+});
+
+
+/*
+|---------------------------------------------------------------
+| Special directive for click outside
+|---------------------------------------------------------------
+|
+| https://stackoverflow.com/questions/36170425/detect-click-outside-element
+*/
+
+Vue.directive('click-outside', {
+    bind: function (el, binding, vnode) {
+      this.event = function (event) {
+        if (!(el == event.target || el.contains(event.target))) {
+          vnode.context[binding.expression](event);
+        }
+      };
+      document.body.addEventListener('click', this.event)
+    },
+    unbind: function (el) {
+      document.body.removeEventListener('click', this.event)
+    },
+  });
+
+/*
+|---------------------------------------------------------------
+| Base entry for vue app
+|---------------------------------------------------------------
+|
+|
+*/
+
+var app = new Vue({
+    el: '#app',
+})
