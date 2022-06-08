3e945358761fd57f67bfee7ba3216e74a4a79c0d
ignitedcms <iamthwee@gmail.com>
2022-06-08 10:15:38 +0100

    u

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,6 @@
 /*
 |---------------------------------------------------------------
-| Search component 
+| Dropdowns 
 | 
 | Components only data must be passed as a function
 | Use slots to repeat child components
@@ -10,29 +10,53 @@
 |
 */
 Vue.component('search',{
-    template: 
-    `
-    <button class="btn rm-btn-styles tooltip-rel" v-on:click="show =!show" v-click-outside="away">
-     <span class="tooltip-highlight"> Click me </span>
-        <div v-if="show" class="tooltip   br drop-shadow fade-in">
-            <slot></slot> 
-        </div>
-    </button>
-    `,
-    data:function(){
+  template: 
+  `
+  <div>
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="type here" v-on:click="show =!show" v-click-outside="away">
+    <div class="small">Some message goes here</div>
+    <div v-if="show" class="search br drop-shadow fade-in">
+          <slot></slot> 
+      </div>
+  </div> 
 
-        return{
-            message: 'Hello',
-            show: false,
-        }
-    },
-    methods:{
-        away: function () {
-            this.show = false;
-        },
-    } 
+  
+  </div>
+  `,
+  data:function(){
+
+      return{
+          message: 'Hello',
+          show: false,
+      }
+  },
+  methods:{
+      away: function () {
+          this.show = false;
+      },
+  } 
 });
 
+Vue.component('item',{
+  props: ['value'],
+  template: 
+  `
+  <div class="row">
+      <div class="col no-margin">
+          <div class="search-item">
+            {{value}}
+          </div>
+      </div>
+  </div>
+  `,
+  // data:function(){
+
+  //     return{
+
+  //     }
+  // }
+});
 
 /*
 |---------------------------------------------------------------
@@ -43,18 +67,18 @@ Vue.component('search',{
 */
 
 Vue.directive('click-outside', {
-    bind: function (el, binding, vnode) {
-      this.event = function (event) {
-        if (!(el == event.target || el.contains(event.target))) {
-          vnode.context[binding.expression](event);
-        }
-      };
-      document.body.addEventListener('click', this.event)
-    },
-    unbind: function (el) {
-      document.body.removeEventListener('click', this.event)
-    },
-  });
+  bind: function (el, binding, vnode) {
+    this.event = function (event) {
+      if (!(el == event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener('click', this.event)
+  },
+  unbind: function (el) {
+    document.body.removeEventListener('click', this.event)
+  },
+});
 
 /*
 |---------------------------------------------------------------
@@ -65,5 +89,5 @@ Vue.directive('click-outside', {
 */
 
 var app = new Vue({
-    el: '#app',
+  el: '#app',
 })
