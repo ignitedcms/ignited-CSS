660f1426de05a7c11c05f2c1ddba6d736fc0354e
ignitedcms <iamthwee@gmail.com>
2022-06-10 13:15:35 +0100

    Update datepicker.js

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -19,34 +19,6 @@ Vue.component('datepicker', {
       <form>
         <label for="year">year</label>
         <select class="form-control" name="year" id="year" >
-          <option value="1990">1990</option>
-          <option value="1991">1991</option>
-          <option value="1992">1992</option>
-          <option value="1993">1993</option>
-          <option value="1994">1994</option>
-          <option value="1995">1995</option>
-          <option value="1996">1996</option>
-          <option value="1997">1997</option>
-          <option value="1998">1998</option>
-          <option value="1999">1999</option>
-          <option value="2000">2000</option>
-          <option value="2001">2001</option>
-          <option value="2002">2002</option>
-          <option value="2003">2003</option>
-          <option value="2004">2004</option>
-          <option value="2005">2005</option>
-          <option value="2006">2006</option>
-          <option value="2007">2007</option>
-          <option value="2009">2009</option>
-          <option value="2010">2010</option>
-          <option value="2011">2011</option>
-          <option value="2012">2012</option>
-          <option value="2013">2013</option>
-          <option value="2014">2014</option>
-          <option value="2015">2015</option>
-          <option value="2016">2016</option>
-          <option value="2017">2017</option>
-          <option value="2018">2018</option>
           <option value="2019">2019</option>
           <option value="2020">2020</option>
           <option value="2021">2021</option>
@@ -65,14 +37,14 @@ Vue.component('datepicker', {
             <th>Sat</th>
           </tr>
         </thead>
-
         <tbody id="calendar-body"></tbody>
       </table>
 
-      <div v-for="i in this.arr">
+      <table>
+      <td v-for="i in this.arr" @click="getIndex(i)">
         {{i}} 
-      </div>
-
+      </td>
+        </table>
 </div>
     `,
     data: function () {
@@ -101,6 +73,10 @@ Vue.component('datepicker', {
         }
     },
     methods: {
+        getIndex(str)
+        {
+            alert(str)
+        },
         next() {
             this.currentYear = (this.currentMonth === 11) ? this.currentYear + 1 : this.currentYear;
             this.currentMonth = (this.currentMonth + 1) % 12;
