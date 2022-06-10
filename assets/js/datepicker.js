d2db9eeacbed8ce5ee05944589e9fef8a7cdecc3
ignitedcms <iamthwee@gmail.com>
2022-06-10 11:29:33 +0100

    u

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -9,29 +9,162 @@
 |
 |
 */
-Vue.component('datepicker',{
-    template: 
-    `
+Vue.component('datepicker', {
+    template: `
+    <div class="temp">
     <div>
-    <input type="text" class="form-control" placeholder="testing dates" v-on:click="show =!show" >
-        <div v-if="show" class="fade-in datepicker">
-            <slot></slot> 
-        </div>
-    </button>
-    </div>
-    `,
-    data:function(){
+        <button id="previous" class="button button-primary" @click="previous()">Previous</button>
+        <button id="next" class="button button-white" @click="next()">Next</button>
+      </div>
+      <form>
+        <label for="year">year</label>
+        <select class="form-control" name="year" id="year" >
+          <option value="1990">1990</option>
+          <option value="1991">1991</option>
+          <option value="1992">1992</option>
+          <option value="1993">1993</option>
+          <option value="1994">1994</option>
+          <option value="1995">1995</option>
+          <option value="1996">1996</option>
+          <option value="1997">1997</option>
+          <option value="1998">1998</option>
+          <option value="1999">1999</option>
+          <option value="2000">2000</option>
+          <option value="2001">2001</option>
+          <option value="2002">2002</option>
+          <option value="2003">2003</option>
+          <option value="2004">2004</option>
+          <option value="2005">2005</option>
+          <option value="2006">2006</option>
+          <option value="2007">2007</option>
+          <option value="2009">2009</option>
+          <option value="2010">2010</option>
+          <option value="2011">2011</option>
+          <option value="2012">2012</option>
+          <option value="2013">2013</option>
+          <option value="2014">2014</option>
+          <option value="2015">2015</option>
+          <option value="2016">2016</option>
+          <option value="2017">2017</option>
+          <option value="2018">2018</option>
+          <option value="2019">2019</option>
+          <option value="2020">2020</option>
+          <option value="2021">2021</option>
+        </select>
+      </form>
+      <h3 id="monthAndYear"></h3>
+      <table id="calendar">
+        <thead>
+          <tr>
+            <th>Sun</th>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thu</th>
+            <th>Fri</th>
+            <th>Sat</th>
+          </tr>
+        </thead>
 
-        return{
-            message: 'Hello',
+        <tbody id="calendar-body"></tbody>
+      </table>
+</div>
+    `,
+    data: function () {
+
+        /*
+        let today = new Date();
+        let currentMonth = today.getMonth();
+        let currentYear = today.getFullYear();
+        let selectYear = document.getElementById("year");
+        // let selectMonth = document.getElementById("month");
+
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        let monthAndYear = document.getElementById("monthAndYear");
+        showCalendar(currentMonth, currentYear);
+
+        */
+
+        return {
+            today: new Date(),
+            currentMonth: this.today.getMonth(),
+            currentYear: this.today.getFullYear(),
+            months:["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], 
             show: false,
         }
     },
-    methods:{
-        away: function () {
-            this.show = false;
+    methods: {
+         next() 
+         {
+            this.currentYear = (this.currentMonth === 11) ? this.currentYear + 1 : this.currentYear;
+            this.currentMonth = (this.currentMonth + 1) % 12;
+            showCalendar(this.currentMonth, this.currentYear);
+         },
+
+         previous() {
+            this.currentYear = (this.currentMonth === 0) ? this.currentYear - 1 : this.currentYear;
+            this.currentMonth = (this.currentMonth === 0) ? 11 : this.currentMonth - 1;
+            showCalendar(this.currentMonth, this.currentYear);
         },
-    } 
+
+         jump() {
+            // this.currentYear = parseInt(selectYear.value);
+            // // currentMonth = parseInt(selectMonth.value);
+            // showCalendar(currentMonth, currentYear);
+        },
+
+         showCalendar(month, year) {
+
+            let firstDay = (new Date(year, month)).getDay();
+            let daysInMonth = 32 - new Date(year, month, 32).getDate();
+        
+            let tbl = document.getElementById("calendar-body"); // body of the calendar
+        
+            // clearing all previous cells
+            tbl.innerHTML = "";
+        
+            // filing data about month and in the page via DOM.
+            monthAndYear.innerHTML = months[month] + " " + year;
+            selectYear.value = year;
+            // selectMonth.value = month;
+        
+            // creating all cells
+            let date = 1;
+            for (let i = 0; i < 6; i++) {
+                // creates a table row
+                let row = document.createElement("tr");
+        
+                //creating individual cells, filing them up with data.
+                for (let j = 0; j < 7; j++) {
+                    if (i === 0 && j < firstDay) {
+                        let cell = document.createElement("td");
+                        let cellText = document.createTextNode("");
+                        cell.appendChild(cellText);
+                        row.appendChild(cell);
+                    }
+                    else if (date > daysInMonth) {
+                        break;
+                    }
+        
+                    else {
+                        let cell = document.createElement("td");
+                        let cellText = document.createTextNode(date);
+                        if (date === this.today.getDate() && year === this.today.getFullYear() && month === this.today.getMonth()) {
+                            cell.classList.add("bg-today");
+                        } // color today's date
+                        cell.appendChild(cellText);
+                        row.appendChild(cell);
+                        date++;
+                    }
+        
+                }
+        
+                tbl.appendChild(row); // appending each row into calendar body.
+            }
+        
+        }
+    }
 });
 
 
@@ -45,17 +178,17 @@ Vue.component('datepicker',{
 
 Vue.directive('click-outside', {
     bind: function (el, binding, vnode) {
-      this.event = function (event) {
-        if (!(el == event.target || el.contains(event.target))) {
-          vnode.context[binding.expression](event);
-        }
-      };
-      document.body.addEventListener('click', this.event)
+        this.event = function (event) {
+            if (!(el == event.target || el.contains(event.target))) {
+                vnode.context[binding.expression](event);
+            }
+        };
+        document.body.addEventListener('click', this.event)
     },
     unbind: function (el) {
-      document.body.removeEventListener('click', this.event)
+        document.body.removeEventListener('click', this.event)
     },
-  });
+});
 
 /*
 |---------------------------------------------------------------
