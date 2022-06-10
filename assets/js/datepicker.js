e9289bb1c71e2d46fbd8e316a147704eae8bb227
ignitedcms <iamthwee@gmail.com>
2022-06-10 12:27:21 +0100

    Update datepicker.js

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -90,79 +90,81 @@ Vue.component('datepicker', {
             today: new Date(),
             currentMonth: new Date().getMonth(),
             currentYear: new Date().getFullYear(),
-            months:["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], 
+            months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
             show: false,
         }
     },
     methods: {
-         next() 
-         {
+        next() {
             this.currentYear = (this.currentMonth === 11) ? this.currentYear + 1 : this.currentYear;
             this.currentMonth = (this.currentMonth + 1) % 12;
             this.showCalendar(this.currentMonth, this.currentYear);
-         },
+        },
 
-         previous() {
+        previous() {
             this.currentYear = (this.currentMonth === 0) ? this.currentYear - 1 : this.currentYear;
             this.currentMonth = (this.currentMonth === 0) ? 11 : this.currentMonth - 1;
             this.showCalendar(this.currentMonth, this.currentYear);
         },
 
-         jump() {
+        jump() {
             // this.currentYear = parseInt(selectYear.value);
             // // currentMonth = parseInt(selectMonth.value);
             // showCalendar(currentMonth, currentYear);
         },
 
-         showCalendar(month, year) {
+        showCalendar(month, year) {
 
             let firstDay = (new Date(year, month)).getDay();
             let daysInMonth = 32 - new Date(year, month, 32).getDate();
-        
+
             let tbl = document.getElementById("calendar-body"); // body of the calendar
-        
+
             // clearing all previous cells
             tbl.innerHTML = "";
-        
+
             // filing data about month and in the page via DOM.
             monthAndYear.innerHTML = this.months[month] + " " + year;
             // selectYear.value = year;
             // selectMonth.value = month;
-        
+
             // creating all cells
             let date = 1;
-            for (let i = 0; i < 6; i++) {
+            for (let i = 0; i < 6; i++) 
+            {
                 // creates a table row
                 let row = document.createElement("tr");
-        
+
                 //creating individual cells, filing them up with data.
-                for (let j = 0; j < 7; j++) {
-                    if (i === 0 && j < firstDay) {
+                for (let j = 0; j < 7; j++) 
+                {
+                    if (i === 0 && j < firstDay) 
+                    {
                         let cell = document.createElement("td");
                         let cellText = document.createTextNode("");
                         cell.appendChild(cellText);
                         row.appendChild(cell);
-                    }
-                    else if (date > daysInMonth) {
+                    } 
+                    else if (date > daysInMonth) 
+                    {
                         break;
                     }
-        
-                    else {
+                    else 
+                    {
                         let cell = document.createElement("td");
                         let cellText = document.createTextNode(date);
-                        if (date === this.today.getDate() && year === this.today.getFullYear() && month === this.today.getMonth()) {
+                        if (date === this.today.getDate() && year === this.today.getFullYear() && month === this.today.getMonth()) 
+                        {
                             cell.classList.add("bg-today");
                         } // color today's date
                         cell.appendChild(cellText);
                         row.appendChild(cell);
                         date++;
                     }
-        
                 }
-        
+
                 tbl.appendChild(row); // appending each row into calendar body.
             }
-        
         }
     }
 });
