ba40f8f2b43535eff92c01191aae4e7250df81b9
ignitedcms <iamthwee@gmail.com>
2022-08-29 16:48:44 +0100

    testing side panel documentation

---
+++ assets/js/side-panel.js
@@ -0,0 +1,44 @@
+/*
+|---------------------------------------------------------------
+| Side panel 
+| 
+| Components only data must be passed as a function
+| Use slots to repeat child components
+| Use props to pass in data MUST use kebab case eg postTitle => post-title 
+|---------------------------------------------------------------
+|
+*/
+Vue.component('sidePanel',{
+    props:['title'],
+    template: 
+    `
+    <div class="side-panel" v-bind:style="{width: menu_width}">
+        {{title}}
+         <div v-on:click="test"> click </div>
+     </div>
+    `,
+    data:function(){
+        return
+        {
+            message: 'Hello',
+            menu_width: '200px',
+            show: false,
+        }
+    },
+    methods:
+    {
+        /*                                                                          
+        |---------------------------------------------------------------            
+        | Here we want to shrink menu width 
+        |---------------------------------------------------------------            
+        |                                                                           
+        */       
+        test()
+        {
+            this.menu_width = '0'
+            document.getElementById("main-panel").style.marginLeft= "0";
+        }
+    }
+});
+
+
