74d86aa4c15025aebca0588e115b6c09438355af
ignitedcms <iamthwee@gmail.com>
2023-11-07 12:23:10 +0000

    improved state

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -10,19 +10,21 @@
 |
 */
 Vue.component('switchIos',{
-    props:['title','value'],
+    props:['name','state'],
     template: 
     `
     <label class="form-switch">
-    <input :name="title" type="checkbox" value:="value" :checked="value" />
-    <i ></i> <div class="switch-text">{{message}}</div>
+
+    <input :name="name" type="hidden"  value="0" />
+    <input :name="name" type="checkbox"  value="1" :checked="state" />
+    <i></i> <div class="switch-text">{{message}}</div>
     </label>
     `,
     data:function(){
 
         return{
-            message: 'Hello',
-            show: this.value,
+            message: 'Yes',
+            show: false,
         }
     }
 });
