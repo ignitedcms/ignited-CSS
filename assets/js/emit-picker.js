b465070266fb9d6c04cbffad7d9e623c14b6af8d
ignitedcms <iamthwee@gmail.com>
2023-12-02 11:42:17 +0000

    u

--- assets/js/emit-picker.js
+++ assets/js/emit-picker.js
@@ -15,17 +15,22 @@ Vue.component('emitpicker',{
     `
     <div class="date-picker">
      <input type="text" :value="value" @input="updateDate($event.target.value)" />
+     <div @click="test('1999-08-06')"> click </div>
     </div>
     `,
     data:function(){
 
         return {
-            content: this.value
+            // content: 'bar'
         }
     },
     methods: {
         updateDate(newValue) {
-            this.$emit('input', 'bar'); // Emit the updated value using v-model
+            this.$emit('input', newValue); // Emit the updated value using v-model
+        },
+        test(idx)
+        {
+            this.updateDate(idx)
         }
     }
 });
