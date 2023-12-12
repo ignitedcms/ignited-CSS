cc894da9542d104f9aafbc480c630858e7bea1eb
ignitedcms <iamthwee@gmail.com>
2023-12-12 11:24:09 +0000

    u

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -24,23 +24,22 @@ Vue.component('modal',{
     props:['button-title','modal-header'],
     template: 
     `
-    <div v-on:keyup.escape="escapePressed()">
+  <div v-on:keyup.escape="escapePressed()">
     <button type="button" class="btn btn-white " v-on:click="open=true" v-click-outside="away">
       {{buttonTitle}} 
     </button>
     <div class="modal" v-show="open" v-on:keyup.escape="escapePressed">
-      <div class="modal-content fade-in-bottom" @click.stop>
+       <div class="modal-content fade-in-bottom" @click.stop>
 
-        <div class="modal-header">
-          <button type="button" class="rm-btn-styles close m-t" v-on:click="open = false">&times;</button>
-          <h5 class="m-t">{{modalHeader}}</h5>
-        </div>
-        <div class="modal-body">
-          <slot></slot>
-        </div>
-       <!-- footer if needed -->
-        </div>
-      </div>
+          <div class="modal-header">
+             <button type="button" class="rm-btn-styles close m-t" v-on:click="open = false">&times;</button>
+             <h5 class="m-t">{{modalHeader}}</h5>
+          </div>
+          <div class="modal-body">
+             <slot></slot>
+          </div>
+          <!-- footer if needed -->
+       </div>
     </div>
   </div>
     `,
