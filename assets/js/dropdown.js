54d421d431055ba1d32e6bf4ce550612a49c34e5
ignitedcms <iamthwee@gmail.com>
2023-12-13 13:22:40 +0000

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -13,7 +13,8 @@ Vue.component('drop-down',{
     props:['button-title'],
     template: 
     `
-    <button v-on:keyup.escape="escapePressed()" type="button" class="btn btn-white drop-shadow pos-rel" v-on:click="show =!show" v-click-outside="away">
+    <button v-on:keyup.escape="escapePressed()" :aria-expanded="arr" type="button" 
+      class="btn btn-white drop-shadow pos-rel" v-on:click="tmp" v-click-outside="away">
         {{buttonTitle}}
         <div v-if="show" class="dropdown br drop-shadow fade-in" @click.stop>
           <focus-trap :active="show">
@@ -27,15 +28,32 @@ Vue.component('drop-down',{
         return{
             message: 'Hello',
             show: false,
+            arr: 'false',
         }
     },
     methods:{
+       tmp()
+       {
+         this.show = ! this.show;
+          if(this.arr == 'true')
+          {
+             this.arr = 'false';
+          }
+          else
+          {
+             this.arr = 'true';
+          }
+
+       },
+
         away: function () {
             this.show = false;
+           this.arr = 'false';
         },
        escapePressed()
        {
          this.show = false;
+          this.arr = 'false';
        }
     } 
 });
