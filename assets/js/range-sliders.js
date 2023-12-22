0d8eb489cfa51fd70befd0182ee73e6deaf294fa
ignitedcms <iamthwee@gmail.com>
2023-12-22 16:35:04 +0000

    Update range-sliders.js

--- assets/js/range-sliders.js
+++ assets/js/range-sliders.js
@@ -1,34 +1,26 @@
-/*
-|---------------------------------------------------------------
-| Switches 
-| 
-| Components only data must be passed as a function
-| Use slots to repeat child components
-| Use props to pass in data MUST use kebab case eg postTitle => post-title 
-|---------------------------------------------------------------
-|
-|
-*/
-Vue.component('rangeSlider',{
-    props:['name','min','max'],
-    template: 
-    `
+Vue.component('range-slider', {
+  props: ['name', 'min', 'max'],
+  template: `
     <div class="slidecontainer">
-        <input :name="name" type="range" v-model="message" 
-          :min="min" :max="max" class="slider2" />
-        <p class="m-t-2">Value: 
-           <span id="demo">
-               {{message}}
-           </span>
-        </p>
+      <input
+        class="slider2"
+        :name="name"
+        type="range"
+        v-model="message"
+        :min="min"
+        :max="max"
+      />
+      <p class="m-t-2">Value:
+        <span id="demo">
+          {{message}}
+        </span>
+      </p>
     </div>
-    `,
-    data:function(){
-
-        return{
-            message: this.min
-        }
-    }
+  `,
+  data() {
+    return {
+      message: this.min
+    };
+  }
 });
-
 
