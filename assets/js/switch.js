2afe93b554b39420d29c3532451de5b1cae6ec63
ignitedcms <iamthwee@gmail.com>
2023-12-22 16:40:17 +0000

    Update switch.js

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -1,36 +1,27 @@
-/*
-|---------------------------------------------------------------
-| Switches 
-| 
-| Components only data must be passed as a function
-| Use slots to repeat child components
-| Use props to pass in data MUST use kebab case eg postTitle => post-title 
-|---------------------------------------------------------------
-|
-|
-*/
-Vue.component('switchIos',{
-    props:['name', 'value'],
-    template: 
-    `
+Vue.component('switchIos', {
+  props: ['name', 'value'],
+  template: `
     <label class="form-switch">
-
-    <input :name="name" type="checkbox"  :checked="value" @change="handleChange" />
-    <i></i> <div class="switch-text no-select">{{message}}</div>
+      <input
+        :name="name"
+        type="checkbox"
+        :checked="value"
+        @change="handleChange"
+      />
+      <i></i>
+      <div class="switch-text no-select">{{message}}</div>
     </label>
-    `,
-    data:function(){
-
-        return{
-            message: 'Yes/no',
-            show: false,
-        }
-    },
-    methods: {
+  `,
+  data() {
+    return {
+      message: 'Yes/no',
+      show: false,
+    };
+  },
+  methods: {
     handleChange(event) {
       this.$emit('input', event.target.checked);
     }
   }
 });
-
 
