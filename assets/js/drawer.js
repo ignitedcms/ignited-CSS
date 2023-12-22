035c293b5b12163c3e41ee2e662f6e246b220239
ignitedcms <iamthwee@gmail.com>
2023-12-22 15:59:59 +0000

    Update drawer.js

--- assets/js/drawer.js
+++ assets/js/drawer.js
@@ -1,45 +1,38 @@
-/*
-|---------------------------------------------------------------
-| Drawer component 
-| 
-| Components only data must be passed as a function
-| Use slots to repeat child components
-| Use props to pass in data MUST use kebab case eg postTitle => post-title 
-|---------------------------------------------------------------
-|
-|
-*/
-Vue.component('drawer',{
-    props:['title'],
-    template: 
-    `
+Vue.component('drawer', {
+  props: ['title'],
+  template: `
     <div>
-       <button  @click="drawer = !drawer"
-          v-click-outside="away"
-          @keyup.escape="escapePressed()"
-          type="submit"
-          class="btn btn-white m-b-2">{{title}}</button>
-       <div v-if="drawer" @click.stop class="drawer fade-in b-l">
-          <focus-trap :active="drawer">
-            <slot></slot>
-          </focus-trap>
-       </div>
+      <button  
+        type="submit"
+        class="btn btn-white m-b-2"
+        @click="toggle"
+        v-click-outside="away"
+        @keyup.escape="escapePressed()"
+      >
+        {{title}}
+      </button>
+      <div v-if="show" @click.stop class="drawer fade-in b-l">
+        <focus-trap :active="show">
+          <slot></slot>
+        </focus-trap>
+      </div>
     </div>
-    `,
-    data:function(){
-        return{
-            drawer: false
-        }
+  `,
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggle(){
+      this.show = !this.show;
     },
-    methods:{
-        away: function () {
-            this.drawer = false;
-        },
-
-       escapePressed()
-       {
-            this.drawer = false;
-       }
-    } 
+    away() {
+      this.show = false;
+    },
+    escapePressed() {
+      this.show = false;
+    },
+  },
 });
 
