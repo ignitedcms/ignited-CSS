e83c7aee0810450698360e9c96d282af1be4ea65
ignitedcms <iamthwee@gmail.com>
2024-01-02 17:53:36 +0000

    u
    added file uploads component

---
+++ assets/js/file-upload.js
@@ -0,0 +1,42 @@
+/*
+|---------------------------------------------------------------
+| Fileupload  component
+|---------------------------------------------------------------
+|
+|
+| @author: IgnitedCMS
+| @license: MIT
+| @version: 1.0
+| @since: 1.0
+|
+*/
+
+Vue.component('file-upload', {
+  props: ['name'],
+  template: `
+  <div>
+      <input 
+       type="file" 
+       :name="name" 
+       :id="name" 
+       @change="handleFileInputChange" 
+       hidden 
+      />
+      <label :for="name" class="form-upload">Choose File</label>
+      <span >{{ fileName }}</span>
+   </div>
+
+  `,
+  data() {
+    return {
+      fileName: '',
+    };
+  },
+  methods: {
+    handleFileInputChange(event) {
+       this.fileName = event.target.files[0].name;
+    },
+  }
+});
+
+
