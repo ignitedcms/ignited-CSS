c3ede03948629f0e73946f161b92b5ca449752d9
ignitedcms <iamthwee@gmail.com>
2024-01-02 15:10:16 +0000

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -10,49 +10,37 @@
 | @since: 1.0
 |
 */
-/*
-|---------------------------------------------------------------
-| Dropdown component
-|---------------------------------------------------------------
-|
-|
-| @author: IgnitedCMS
-| @license: MIT
-| @version: 1.0
-| @since: 1.0
-|
-*/
 Vue.component('drop-down', {
   props: ['buttonTitle'],
   template: `
-   <div>
-    <button
-      :id="'dropdown-' + uniqueId"
-      type="button"
-      aria-haspopup="menu"
-      :aria-expanded="arr"
-      class="btn btn-white pos-rel"
-      @keyup.esc="escapePressed"
-      @click="toggle"
-      v-click-outside="away"
-    >
-      {{ buttonTitle }}
-      
-    </button>
+    <div>
+      <button
+        :id="'dropdown-' + uniqueId"
+        type="button"
+        aria-haspopup="menu"
+        :aria-expanded="arr"
+        class="btn btn-white pos-rel"
+        @keyup.esc="escapePressed"
+        @click="toggle"
+        v-click-outside="away"
+      >
+        {{ buttonTitle }}
+      </button>
       <div
-        v-if="show"
+        v-show="show"
+        tabindex="-1"
         role="menu"
         :aria-labelledby="'dropdown-' + uniqueId"
         class="pos-abs dropdown br drop-shadow fade-in"
-      @keydown.down.prevent="navigate('down')"
-      @keydown.up.prevent="navigate('up')"
-      @keydown.enter.prevent="selectItem"
+        @keydown.down.prevent="navigate('down')"
+        @keydown.up.prevent="navigate('up')"
+        @keydown.enter.prevent="selectItem"
         @click.stop
+        ref="datepicker"
       >
-          <slot></slot>
+        <slot></slot>
       </div>
-
-   </div>
+    </div>
   `,
   data() {
     return {
@@ -66,6 +54,10 @@ Vue.component('drop-down', {
     toggle() {
       this.show = !this.show;
       this.arr = this.show ? 'true' : 'false';
+
+      this.$nextTick(() => {
+        this.$refs.datepicker.focus();
+      });
     },
     away() {
       this.show = false;
@@ -77,23 +69,19 @@ Vue.component('drop-down', {
       this.arr = 'false';
       this.selectedIndex = -1; // Reset selected index on escape
     },
-     navigate(direction) {
-        console.log('pres');
-  if (this.show) {
-    const items = this.$el.querySelectorAll('.dropdown-item');
-    console.log('Items:', items.length); // Check if items are being properly selected
-    if (direction === 'down') {
-      this.selectedIndex = (this.selectedIndex + 1) % items.length;
-    } else if (direction === 'up') {
-      this.selectedIndex = this.selectedIndex <= 0 ? items.length - 1 : this.selectedIndex - 1;
-    }
-    if (items.length > 0) {
-      console.log('Selected Index:', this.selectedIndex); // Check the selected index value
-      items[this.selectedIndex].focus(); // Set focus on the selected item if it exists
-    }
-  }
-},
-
+    navigate(direction) {
+      if (this.show) {
+        const items = this.$el.querySelectorAll('.dropdown-item');
+        if (direction === 'down') {
+          this.selectedIndex = (this.selectedIndex + 1) % items.length;
+        } else if (direction === 'up') {
+          this.selectedIndex = this.selectedIndex <= 0 ? items.length - 1 : this.selectedIndex - 1;
+        }
+        if (items.length > 0) {
+          items[this.selectedIndex].focus(); // Set focus on the selected item if it exists
+        }
+      }
+    },
     selectItem() {
       if (this.show && this.selectedIndex !== -1) {
         const items = this.$el.querySelectorAll('.dropdown-item');
@@ -101,10 +89,9 @@ Vue.component('drop-down', {
         // Perform action based on the selected item (e.g., emit an event)
         this.$emit('item-selected', selectedItem.textContent);
         this.toggle(); // Close dropdown after selection
+      } else {
+        this.toggle();
       }
-       else{
-          this.toggle();
-       }
     },
   },
 });
@@ -123,3 +110,4 @@ Vue.component('item', {
     </div>
   `,
 });
+
