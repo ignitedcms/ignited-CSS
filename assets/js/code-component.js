90436a2c5555ab9a1bdf210c02ee566fd5d4fb1a
ignitedcms <iamthwee@gmail.com>
2024-01-12 20:17:31 +0000

    u

--- assets/js/code-component.js
+++ assets/js/code-component.js
@@ -15,20 +15,45 @@ Vue.component('code-component', {
   props: ['title'],
   template: `
     <div>
-    code
+      <tabs>
+        <tab-item title="Preview">
+          <div class="v-a h-a bg-light-grey" style="min-height:300px;">
+            <!-- actual code unescaped here -->
+          </div>
+        </tab-item>
+        <tab-item title="Code">
+          <div class="pos-rel">
+            <pre class="code" style="height:300px;">
+test
+            </pre>
+            <span @click="copyCode" class="text-white icon pos-abs hand" style="top:10px; right:15px;">
+              <i data-feather="clipboard"></i>
+            </span>
+          </div>
+          <textarea rows="4" :id="uniqueId" style="display:none;">
+test
+          </textarea>
+        </tab-item>
+      </tabs>
     </div>
   `,
   data() {
     return {
-      uniqueId: Math.random().toString(36).substring(2) // Generate a unique ID
+      uniqueId: Math.random().toString(36).substring(2), // Generate a unique ID
     };
   },
   methods: {
-    test() {
-      alert('foo');
+    copyCode() {
+      // Get the text field
+      var copyText = document.getElementById(this.uniqueId);
+
+      // Select the text field
+      copyText.select();
+      copyText.setSelectionRange(0, 99999); // For mobile devices
+
+      // Copy the text inside the text field
+      navigator.clipboard.writeText(copyText.value);
     },
-    
   },
 });
-
 
